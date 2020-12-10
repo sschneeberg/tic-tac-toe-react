d1ddaab29a38b6b00787a4b91400f3efbe0f36bd
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-10 10:14:05 -0500

    Lift up state again

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,51 +3,29 @@ import Square from './Square';
 import '../index.css';
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null)
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
-    }
-
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                key={i}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
-    render() {
-        const status = 'Next player: X';
+    renderSquares() {
+        let squares = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                let ind = i * 3 + j;
+                squares.push(this.renderSquare(ind));
+            }
+        }
+        return squares;
+    }
 
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+    render() {
+        return <div className="board">{this.renderSquares()}</div>;
     }
 }
 
